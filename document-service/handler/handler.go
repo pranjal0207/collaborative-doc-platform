90e58048de57cd10d3ea03a053ce87c0e104ee9e
Pranjal Rane <rane.pr@northeastern.edu>
2024-07-20 22:56:02 -0400

    add create and get document endpoints

--- document-service/handler/handler.go
+++ document-service/handler/handler.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"context"
+
 	"pranjal0207/collaborative-doc-platform/document-service/model"
 	pb "pranjal0207/collaborative-doc-platform/document-service/proto"
+
+	"github.com/google/uuid"
 )
 
 type Server struct {
@@ -12,24 +15,51 @@ type Server struct {
 }
 
 func (s *Server) CreateDocument(ctx context.Context, req *pb.CreateDocumentRequest) (*pb.CreateDocumentResponse, error) {
+	documentID := uuid.New().String()
+
+	document := model.Document{
+		Title:      req.Title,
+		Author:     req.Author,
+		Content:    "",
+		DocumentID: documentID,
+	}
+
+	err := s.DocumentModel.CreateDocument(ctx, &document)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.CreateDocumentResponse{
-		DocumentId: "SOME ID",
+		DocumentId: documentID,
 	}, nil
 }
 
 func (s *Server) GetDocument(ctx context.Context, req *pb.GetDocumentRequest) (*pb.GetDocumentResponse, error) {
+	document, err := s.DocumentModel.GetDocumentByID(ctx, req.DocumentId)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.GetDocumentResponse{
-		DocumentId: "SOME ID",
-		Title: "Title",
-    	Content:"Content",
-    	Author:"Authot",
-    	Versions: []string {"version"},
+		DocumentId: document.DocumentID,
+		Title:      document.Title,
+		Content:    document.Content,
+		Author:     document.Author,
+		Versions:   document.Versions,
 	}, nil
 }
 
 func (s *Server) DeleteDocument(ctx context.Context, req *pb.DeleteDocumentRequest) (*pb.DeleteDocumentResponse, error) {
+	err := s.DocumentModel.DeleteDocumentByID(ctx, req.DocumentId)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.DeleteDocumentResponse{
-		DocumentId: "SOME ID",
+		DocumentId: req.DocumentId,
 	}, nil
 }
 
@@ -44,4 +74,3 @@ func (s *Server) ListDocumentVersions(ctx context.Context, req *pb.ListDocumentV
 		Versions: []string{"version1", "version2"},
 	}, nil
 }
-
--- document-service/model/model.go
+++ document-service/model/model.go
@@ -1,9 +1,94 @@
 package model
 
-import "github.com/aws/aws-sdk-go-v2/service/dynamodb"
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
 
 type DocumentModel struct {
 	DynamoDB  *dynamodb.Client
 	TableName string
 }
 
+type Document struct {
+	DocumentID string   `dynamodbav:"document_id"`
+	Title      string   `dynamodbav:"title"`
+	Content    string   `dynamodbav:"content"`
+	Author     string   `dynamodbav:"author"`
+	Versions   []string `dynamodbav:"versions"`
+	Timestamp  string   `dynamodbav:"timestamp"`
+}
+
+func (d *DocumentModel) CreateDocument(ctx context.Context, document *Document) error {
+	av, err := attributevalue.MarshalMap(document)
+
+	if err != nil {
+		return fmt.Errorf("failed to create new User %w", err)
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: &d.TableName,
+	}
+
+	_, err = d.DynamoDB.PutItem(ctx, input)
+
+	if err != nil {
+		return fmt.Errorf("failed to put item: %w", err)
+	}
+
+	return nil
+}
+
+func (d *DocumentModel) GetDocumentByID(ctx context.Context, documentId string) (*Document, error) {
+	input := &dynamodb.GetItemInput{
+		TableName: &d.TableName,
+		Key: map[string]types.AttributeValue{
+			"document_id": &types.AttributeValueMemberS{Value: documentId},
+		},
+	}
+
+	result, err := d.DynamoDB.GetItem(ctx, input)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan items: %w", err)
+	}
+
+	if result.Item != nil {
+		return nil, fmt.Errorf("document not found")
+	}
+
+	var document Document
+
+	err = attributevalue.UnmarshalMap(result.Item, &document)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal document: %w", err)
+	}
+
+	return &document, nil
+}
+
+func (d *DocumentModel) DeleteDocumentByID(ctx context.Context, documentId string) error {
+	input := &dynamodb.DeleteItemInput{
+		TableName: &d.TableName,
+		Key: map[string]types.AttributeValue{
+			"document_id": &types.AttributeValueMemberS{Value: documentId},
+		},
+	}
+
+	_, err := d.DynamoDB.DeleteItem(ctx, input)
+
+	if err != nil {
+		return fmt.Errorf("failed to delete item: %w", err)
+	}
+
+	return nil
+}
+
+func (d *DocumentModel) UpdateDocumentByID(ctx context.Context, documentId string) error {
+	return nil
+}
